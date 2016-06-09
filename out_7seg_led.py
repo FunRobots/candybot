b8d75c38fe71db05a44668e46d7bc134edd6f31d
Mike <mnrozhkov@gmail.com>
2016-06-09 08:48:49 +0300

    Change pin numbers for 7seg LED

--- out_7seg_led.py
+++ out_7seg_led.py
@@ -1,10 +1,9 @@
 import RPi.GPIO as GPIO
-import time
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD) # GPIO.BOARD is used here!!!
 GPIO.setwarnings(False)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-segments = (24,12,19,21,23,22,15,11)
+segments = (24,10,19,21,23,22,15,11)
 
 for segment in segments:
     GPIO.setup(segment, GPIO.OUT)
@@ -16,7 +15,17 @@ for digit in digits:
     GPIO.setup(digit, GPIO.OUT)
     GPIO.output(digit, 1)
 
-num = {' ':(0,0,0,0,0,0,0),
+
+
+def display_4digits(code):
+    """ 
+    Params:
+        digits: string of 4 digits code or 4-digit number
+    Returns: 
+        High output on appropriate 7-segment LED
+    """
+
+    num = {' ':(0,0,0,0,0,0,0),
     '0':(1,1,1,1,1,1,0),
     '1':(0,1,1,0,0,0,0),
     '2':(1,1,0,1,1,0,1),
@@ -28,13 +37,6 @@ num = {' ':(0,0,0,0,0,0,0),
     '8':(1,1,1,1,1,1,1),
     '9':(1,1,1,1,0,1,1)}
 
-def display_4digits(code):
-    """ 
-    Params:
-        digits: string of 4 digits code or 4-digit number
-    Returns: 
-        High output on appropriate 7-segment LED
-    """
     if code:
         s = str(digits).rjust(4)
         print(s)
@@ -43,9 +45,3 @@ def display_4digits(code):
         for loop in range(0,7):
             print(GPIO.output(segments[loop], num[s[digit]][loop]))
 
-code = 5423
-try:
-    while True:
-        display_4digits(code)
-except KeyboardInterrupt:
-    GPIO.cleanup()
